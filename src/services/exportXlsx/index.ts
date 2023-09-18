140c153d4a6d6479dbc32af5cfa7b5d50e917fec
Anderson Roque <andersonbarrinha18@gmail.com>
2023-09-17 22:32:17 -0300

    Fix: https

--- src/services/exportXlsx/index.ts
+++ src/services/exportXlsx/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function exportXlsx(file: any) {
   const response = await axios
-    .post("http://api-export-shop.onrender.com", file)
+    .post("https://api-export-shop.onrender.com", file)
     .then((res) => res)
     .catch((err) => err.response);
 
