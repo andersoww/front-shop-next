73e8105582821f8bd86f2ac6f9c57a536afd429c
Anderson Roque <andersonbarrinha18@gmail.com>
2023-10-01 22:11:02 -0300

    Fix: env

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.NEXT_URL_PUBLIC_URL,
+  },
 };
 
 module.exports = withPWA(nextConfig);
